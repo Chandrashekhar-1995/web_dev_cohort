f68f9430d0bf28c2d7dbd637f76de09ff24b67c7
Chandrashekhar-1995 <jmbd.group@gmail.com>
2025-04-06 22:42:47 +0530

    create utils for send mail in mega project

---
+++ Regular_Live_classes_Phase_2/03_mega_backend_project/src/utils/mail.js
@@ -0,0 +1,88 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendMail = async(options) =>{
+    const mailGenerator = new Mailgen({
+        theme: 'default',
+        product: {
+            name: 'Task Manager',
+            link: 'https://mailgen.js/'
+        },
+    });
+
+    var emailText = mailGenerator.generatePlaintext(options.mailGenContent);
+    var emailHtml = mailGenerator.generate(options.mailGenContent);
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        secure: false, // true for port 465, false for other ports
+        auth: {
+          user: process.env.MAILTRAP_SMTP_USER,
+          pass: process.env.MAILTRAP_SMTP_PASS,
+        },
+      });
+
+
+      const mail = {
+        from: process.env.MAILTRAP_SMTP_SENDER_EMAIL,
+        to: options.email,
+        subject: options.subject,
+        text: emailText,
+        html: emailHtml,
+      };
+
+      try {
+        await transporter.sendMail(mail)
+      } catch (error) {
+        console.error("Email sending failed", error)
+      }
+}
+
+
+const emailVerificarionMailGenContent = (username, verificationUrl) => {
+    return {
+        body:{
+            name: username,
+            intro: "Welcome to Task Manager! We're very excited to have you on board.",
+            action: {
+                instructions: "To get started with our App, please click here:",
+                button: {
+                    color: "#22BC66",
+                    text: "Confirm your account",
+                    link: verificationUrl
+                },
+            },
+            outro: "Need help, or have questions? Just reply to this email, we'd love to help."
+        }
+    }
+}
+
+
+const forgotPasswordMailGenContent = (username, passwordResetUrl) => {
+    return {
+        body:{
+            name: username,
+            intro: "We got a request to reset your password",
+            action: {
+                instructions: "To change your password, please click here:",
+                button: {
+                    color: "#22BC66",
+                    text: "Reset Password",
+                    link: passwordResetUrl
+                },
+            },
+            outro: "Need help, or have questions? Just reply to this email, we'd love to help."
+        }
+    }
+}
+
+
+sendMail({
+    email:user.email,
+    subject:"test",
+    mailGenContent: emailVerificarionMailGenContent(
+        username,
+        `` // verification link
+    )
+})
